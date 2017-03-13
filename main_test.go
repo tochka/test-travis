cadb1aa99adea4a6225c902409d5130e40fab6b2
Vladimir Vinogradov <vinogradovve@gmail.com>
2017-03-13 22:41:36 +0500

    Create main_test.go

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+func TestSum_ReturnVal(t *testing.T){
+  const (
+    a=4 
+    b=5
+    expected=9
+   )
+   actual:=Sum(a,b)
+   if actual != expected {
+    t.FatalNow()
+    
+   }
+  }
